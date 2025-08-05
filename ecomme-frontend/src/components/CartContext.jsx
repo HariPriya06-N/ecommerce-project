bd370059d8c678096bb5138a0a110f1a68bd780f
HariPriya06-N <natukulaharipriya@gmail.com>
2025-08-05 13:00:26 +0530

    removed comments

--- ecomme-frontend/src/components/CartContext.jsx
+++ ecomme-frontend/src/components/CartContext.jsx
@@ -1,49 +1,5 @@
 
-// import { createContext, useContext, useState, useEffect } from "react";
 
-// const CartContext = createContext();
-
-// export const CartProvider = ({ children }) => {
-//   const [cartItems, setCartItems] = useState(() => {
-//     const storedCart = localStorage.getItem("cart");
-//     return storedCart ? JSON.parse(storedCart) : [];
-//   });
-
-  
-//   useEffect(() => {
-//     localStorage.setItem("cart", JSON.stringify(cartItems));
-//   }, [cartItems]);
-
-//   const addToCart = (item, quantity = 1) => {
-//     setCartItems((prev) => {
-//       const existing = prev.find((i) => i.pid === item.pid);
-//       if (existing) {
-//         return prev.map((i) =>
-//           i.pid === item.pid ? { ...i, qty: i.qty + quantity } : i
-//         );
-//       }
-//       return [...prev, { ...item, qty: quantity }];
-//     });
-//   };
-
-//   const removeFromCart = (pid) => {
-//     setCartItems((prev) => prev.filter((item) => item.pid !== pid));
-//   };
-
-//   const clearCart = () => {
-//     setCartItems([]);
-//   };
-
-//   return (
-//     <CartContext.Provider
-//       value={{ cartItems, addToCart, removeFromCart, clearCart }}
-//     >
-//       {children}
-//     </CartContext.Provider>
-//   );
-// };
-
-// export const useCart = () => useContext(CartContext);
 import { createContext, useContext, useState, useEffect } from "react";
 
 const CartContext = createContext();
@@ -59,11 +15,11 @@ export const CartProvider = ({ children }) => {
   }, [cartItems]);
 
   const addToCart = (item, quantity = 1) => {
-    // Add a uniqueId to make each entry unique even if same product
+   
     const itemWithId = {
       ...item,
       qty: quantity,
-      uniqueId: Date.now() + Math.random() // unique key for rendering
+      uniqueId: Date.now() + Math.random() 
     };
 
     setCartItems((prev) => {
